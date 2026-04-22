33b7ab040cfc2f548924ca408b9fff8007980a13
estebanzonta <esteban.zonta@gmail.com>
2026-04-21 22:34:57 -0300

    se corrige error 500

--- api/assets/[folder]/[filename].ts
+++ api/assets/[folder]/[filename].ts
@@ -1,4 +1,4 @@
-import { handleParamRequest } from '../../../../vercel-api/segment-handler.js';
+import { handleParamRequest } from '../../../vercel-api/segment-handler.js';
 
 export default function handler(req: any, res: any) {
   return handleParamRequest('/assets', ['folder', 'filename'], req, res);
